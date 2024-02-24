69fe60a823c746a63f004adcb152a41dd1ccb36c
Matteo Crespi <crespi.matteo@icloud.com>
2024-02-24 15:06:14 +0100

    feat: richer sample

--- app/App.swift
+++ app/App.swift
@@ -3,41 +3,123 @@ import ComposableArchitecture
 import IGListKit
 import AsyncDisplayKit
 import IGListSwiftKit
+import Combine
 
 @UIApplicationMain
 final class ApplicationDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    let store = Store(initialState: .init(), reducer: Feature.init)
     window = UIWindow()
-    window?.rootViewController = UIViewController()
+    window?.rootViewController = ViewController(store: store)
     window?.makeKeyAndVisible()
-    print(SectionController.self)
     return true
   }
 }
 
-struct Feature: Reducer {
-  typealias Action = String
-  typealias State = String
-  var body: some Reducer<State, Action> {
-    Reduce { _, _ in .none }
+@Reducer
+struct Feature {
+  @ObservableState
+  struct State {
+    var count = 1
+  }
+  
+  enum Action {
+    case increment
+  }
+  
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .increment:
+        state.count += 1
+        return .none
+      }
+    }
   }
 }
 
-struct SectionControllerConfiguration: Hashable, ListIdentifiable {
-  let rawValue: String
-  var diffIdentifier: NSObjectProtocol {
-    rawValue as NSString
+class ViewController: ASDKViewController<ASCollectionNode> {
+  private lazy var adapter = ListAdapter(updater: ListAdapterUpdater(), viewController: self)
+  private let store: StoreOf<Feature>
+  private var cancellables: [AnyCancellable] = []
+
+  init(store: StoreOf<Feature>) {
+    self.store = store
+    
+    let collectionLayout = UICollectionViewFlowLayout()
+    let collectionNode = ASCollectionNode(collectionViewLayout: collectionLayout)
+    collectionNode.backgroundColor = .systemBackground
+    super.init(node: collectionNode)
+
+    adapter.setASDKCollectionNode(node)
+    adapter.dataSource = self
+    
+    store.publisher.sink { [weak self] _ in
+      self?.adapter.performUpdates(animated: true)
+    }.store(in: &cancellables)
   }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension ViewController: ListAdapterDataSource {
+  func listAdapter(_: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
+    let controller = SectionController()
+    controller.delegate = self
+    return controller
+  }
+
+  func objects(for _: ListAdapter) -> [ListDiffable] {
+    (1 ... store.count).map { number in
+      SectionControllerConfiguration(text: number.description).diffable()
+    }
+  }
+
+  func emptyView(for _: ListAdapter) -> UIView? {
+    nil
+  }
+}
+
+extension ViewController: SectionControllerDelegate {
+  func sectionControllerDidTap(_ sectionController: SectionController) {
+    store.send(.increment)
+  }
+}
+
+
+struct SectionControllerConfiguration: Hashable, ListIdentifiable {
+  let text: String
+  var diffIdentifier: NSObjectProtocol {
+    text as NSString
+  }
+}
+
+protocol SectionControllerDelegate: AnyObject {
+  func sectionControllerDidTap(_ sectionController: SectionController)
 }
 
 final class SectionController: ListValueSectionController<SectionControllerConfiguration> {
+  weak var delegate: SectionControllerDelegate?
+  
   override func numberOfItems() -> Int {
     1
   }
 
   func nodeBlockForItem(at index: Int) -> ASCellNodeBlock {
-    { ASCellNode() }
+    let text = value.text
+    return {
+      let node = ASTextCellNode()
+      node.backgroundColor = .systemYellow
+      node.text = text
+      return node
+    }
+  }
+  
+  override func didSelectItem(at index: Int) {
+    delegate?.sectionControllerDidTap(self)
   }
 }
 
